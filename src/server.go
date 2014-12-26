f49740c5aa6922213cdf0d69d267978c930e694b
Gleicon Moraes <gleicon@gmail.com>
2014-12-26 20:56:48 -0200

    here we go again 2

--- src/server.go
+++ src/server.go
@@ -26,12 +26,16 @@ func (ms MemcachedProtocolServer) Close() {
 
 func (ms MemcachedProtocolServer) Start() {
 	var err error
+	var id int
+
+	id = 0
 	ms.listener, err = net.Listen("tcp", ms.address)
 	if err == nil {
 		for {
 			if conn, err := ms.listener.Accept(); err == nil {
 				conn.SetDeadline(time.Now().Add(time.Duration(10) * time.Second))
-				go ms.handle(conn)
+				go ms.handle(conn, id)
+				id++
 			} else {
 				log.Print(err.Error())
 			}
@@ -40,19 +44,19 @@ func (ms MemcachedProtocolServer) Start() {
 		log.Fatal(err.Error())
 	}
 }
-func (ms MemcachedProtocolServer) sendMessage(conn net.Conn, msg string, noreply bool) {
+func (ms MemcachedProtocolServer) sendMessage(conn net.Conn, msg string, noreply bool, id int) {
 	if noreply == true {
-		log.Printf("NOREPLY RESPONSE: %s\n", msg)
+		log.Printf("%d NOREPLY RESPONSE: %s\n", id, msg)
 		return
 	}
 	m := fmt.Sprintf("%s\r\n", msg)
 	conn.Write([]byte(m))
-	log.Printf("RESPONSE: %s\n", m)
+	log.Printf("%d RESPONSE: %s\n", id, m)
 }
 
-func (ms MemcachedProtocolServer) handle(conn net.Conn) {
+func (ms MemcachedProtocolServer) handle(conn net.Conn, id int) {
+	log.Printf("Spawning new goroutine %d\n", id)
 	defer conn.Close()
-Loop:
 	for {
 		noreply := false
 		scanner := bufio.NewScanner(conn)
@@ -64,22 +68,21 @@ Loop:
 			continue
 		}
 
-		log.Printf("REQUEST: %s", line)
+		log.Printf("%d REQUEST: %s", id, line)
 		args := strings.Split(line, " ")
 		cmd := strings.ToLower(args[0])
-		log.Println(args)
 
 		if args[len(args)-1] == "noreply" {
 			noreply = true
 		} else {
 			noreply = false
 		}
-		log.Printf("NOREPLY STATUS: %b\n", noreply)
+		log.Printf("%d NOREPLY STATUS: %b\n", id, noreply)
 		switch true {
 		case cmd == "get":
 			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			}
 			for _, arg := range args[1:] {
 				if arg == " " || arg == "" {
@@ -98,71 +101,73 @@ Loop:
 					conn.Write([]byte("\r\n"))
 				}
 			}
-			ms.sendMessage(conn, "END", noreply)
+			ms.sendMessage(conn, "END", noreply, id)
 
 		case cmd == "set":
 			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			}
 			// retrieve body
 			scanner.Scan()
 			body := scanner.Bytes()
 			if len(body) == 0 {
-				ms.sendMessage(conn, "ERROR", false)
+				ms.sendMessage(conn, "ERROR", false, id)
 			} else {
 				ms.vdb.Set([]byte(args[1]), []byte(body))
-				ms.sendMessage(conn, "STORED", noreply)
+				ms.sendMessage(conn, "STORED", noreply, id)
 			}
-			continue
+			break
 
 		case cmd == "replace":
 			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			}
 			// retrieve body
 			scanner.Scan()
 			body := scanner.Bytes()
 			if len(body) == 0 {
-				ms.sendMessage(conn, "ERROR", noreply)
-				continue
+				ms.sendMessage(conn, "ERROR", noreply, id)
+				break
 			} else {
 				err := ms.vdb.Replace([]byte(args[1]), []byte(body))
 				if err != nil {
 					log.Println(err)
-					ms.sendMessage(conn, "NOT_STORED", noreply)
+					ms.sendMessage(conn, "NOT_STORED", noreply, id)
 				} else {
-					ms.sendMessage(conn, "STORED", noreply)
+					ms.sendMessage(conn, "STORED", noreply, id)
 				}
 			}
+			break
 
 		case cmd == "add":
 			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			}
 
 			// retrieve body
 			scanner.Scan()
 			body := scanner.Bytes()
 			if len(body) == 0 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			} else {
 				err := ms.vdb.Add([]byte(args[1]), []byte(body))
 				if err != nil {
 					log.Println(err)
-					ms.sendMessage(conn, "NOT_STORED", noreply)
+					ms.sendMessage(conn, "NOT_STORED", noreply, id)
 				} else {
-					ms.sendMessage(conn, "STORED", noreply)
+					ms.sendMessage(conn, "STORED", noreply, id)
 				}
 			}
+			break
 
 		case cmd == "quit":
 			if len(args) > 1 {
-				ms.sendMessage(conn, "ERROR", false)
-				continue
+				ms.sendMessage(conn, "ERROR", false, id)
+				break
 			} else {
 				conn.Close()
 				return
@@ -170,33 +175,33 @@ Loop:
 
 		case cmd == "version":
 			if len(args) > 1 {
-				ms.sendMessage(conn, "ERROR", false)
+				ms.sendMessage(conn, "ERROR", false, id)
 			} else {
-				ms.sendMessage(conn, "VERSION BEANO", false)
+				ms.sendMessage(conn, "VERSION BEANO", false, id)
 			}
-			continue
+			break
 
 		case cmd == "flush_all":
 			ms.vdb.Flush()
-			ms.sendMessage(conn, "OK", noreply)
-			break Loop
+			ms.sendMessage(conn, "OK", noreply, id)
+			break
 
 		case cmd == "verbosity":
-			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", false)
+			if len(args) < 2 || len(args) > 3 {
+				ms.sendMessage(conn, "ERROR", false, id)
 			} else {
-				ms.sendMessage(conn, "OK", noreply)
+				ms.sendMessage(conn, "OK", noreply, id)
 			}
-			continue
+			break
 
 		case cmd == "delete":
 			if len(args) < 2 {
-				ms.sendMessage(conn, "ERROR", noreply)
-				continue
+				ms.sendMessage(conn, "ERROR", noreply, id)
+				break
 			}
 			if len(args) > 3 {
-				ms.sendMessage(conn, "ERROR", noreply)
-				continue
+				ms.sendMessage(conn, "ERROR", noreply, id)
+				break
 			}
 
 			deleted, err := ms.vdb.Delete([]byte(args[1]), true)
@@ -204,16 +209,16 @@ Loop:
 				log.Println(err)
 			}
 			if deleted == true {
-				ms.sendMessage(conn, "DELETED", noreply)
+				ms.sendMessage(conn, "DELETED", noreply, id)
 			} else if deleted == false {
-				ms.sendMessage(conn, "NOT_FOUND", noreply)
+				ms.sendMessage(conn, "NOT_FOUND", noreply, id)
 			}
-			continue
+			break
 
 		default:
 			log.Printf("NOT IMPLEMENTED: %s\n", args[0])
-			ms.sendMessage(conn, "ERROR", false)
-			continue
+			ms.sendMessage(conn, "ERROR", false, id)
+			break
 
 		}
 	}
